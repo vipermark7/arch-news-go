9a7016fe05e0c0f3f6726e6dd0cee6b6e68a12d6
Shaffan Mustafa <shaffan1996@gmail.com>
2023-06-04 00:10:42 -0500

    can see all posts from feed. need to remove html tags

--- news.go
+++ news.go
@@ -4,16 +4,25 @@ import (
   "fmt"
   "github.com/mmcdole/gofeed"
   "html"
+  "strings"
 )
 
 
 func main() {
-	var parsedNews *gofeed.Feed = parseNews()
-  fmt.Println(html.UnescapeString(parsedNews.Items[0].Description))
+  var parsedNews *gofeed.Feed = parseNews()
+  for _, post := range parsedNews.Items {
+    printNewsItem(post)
+  }
 }
 
-func parseNews() *gofeed.Feed {
+func printNewsItem(item *gofeed.Item) {
+  fmt.Println(html.UnescapeString(item.Title))
+  for _, word := range strings.Split(item.Description, " ") {
+    fmt.Print(html.UnescapeString(word) + " ")
+  }
+}  
 
+func parseNews() *gofeed.Feed {
   ARCH_NEWS_URL :=  "https://archlinux.org/feeds/news/"
   fp := gofeed.NewParser()
   parsed, err := fp.ParseURL(ARCH_NEWS_URL)
